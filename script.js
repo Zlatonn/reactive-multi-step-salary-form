fc8d038c1eb7917d0838427399f365b24b3da9c4
Zlatonn <tonkar2541@hotmail.com>
2024-10-18 17:55:15 +0700

    add intial process for create pages element

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 import { atom } from "https://unpkg.com/nanostores";
 
-let pageStores = ["Income", "Expenses", "Summary"];
+// -------------------- Initial Process (create pages) --------------------
+
+let pageStores = [
+  {
+    name: "Income",
+    detail: ["Salary", "Bonus", "Bonus", "Investment", "Other"],
+  },
+  {
+    name: "Expenses",
+    detail: ["Housing", "Food", "Transportation", "Heathcare", "Education", "Other"],
+  },
+  {
+    name: "Summary",
+    detail: [],
+  },
+];
 
 createBullet();
+createCard();
+createHandlerButton();
 
 function createBullet() {
   const stepContainer = document.querySelector(".step-container");
@@ -17,22 +34,119 @@ function createBullet() {
     const stepDetail = document.createElement("div");
     stepDetail.classList.add("step-detail");
 
-    const stepLine = document.createElement("div");
-    stepLine.classList.add("step-line");
-
     stepContainer.appendChild(stepBox);
 
     stepBox.appendChild(stepNum);
     stepNum.innerHTML = i + 1;
 
     stepBox.appendChild(stepDetail);
-    stepDetail.innerHTML = e;
+    stepDetail.innerHTML = e.name;
 
     if (i < pageStores.length - 1) {
+      const stepLine = document.createElement("div");
+      stepLine.classList.add("step-line");
       stepContainer.appendChild(stepLine);
     }
   });
 }
+
+function createCard() {
+  const cardContainer = document.querySelector(".card-container");
+
+  pageStores.forEach((e, i) => {
+    const cardBox = document.createElement("div");
+    cardBox.classList.add("card-box");
+
+    const cardTitle = document.createElement("p");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerHTML = e.name;
+
+    const cardDetail = document.createElement("div");
+    cardDetail.classList.add("card-detail");
+
+    const leftBox = document.createElement("div");
+    leftBox.classList.add("left-box");
+
+    const rightBox = document.createElement("div");
+    rightBox.classList.add("right-box");
+
+    cardContainer.appendChild(cardBox);
+
+    cardBox.appendChild(cardTitle);
+    cardBox.appendChild(cardDetail);
+
+    cardDetail.appendChild(leftBox);
+    cardDetail.appendChild(rightBox);
+
+    // Insert Detail Box
+
+    if (e.detail && e.detail.length > 0) {
+      e.detail.forEach((d) => {
+        const subDetailLeft = document.createElement("div");
+        subDetailLeft.classList.add("sub-detail");
+
+        const text = document.createElement("p");
+        text.innerHTML = `${d} <span>/month</span>`;
+
+        const input = document.createElement("input");
+        input.type = "number";
+        input.value = 0;
+        input.min = "0";
+        input.id = `${d.toLowerCase()}-input`;
+
+        subDetailLeft.appendChild(text);
+        subDetailLeft.appendChild(input);
+
+        leftBox.appendChild(subDetailLeft);
+      });
+    } else {
+      for (let count = 0; count < 2; count++) {
+        const subDetailLeft = document.createElement("div");
+        subDetailLeft.classList.add("sub-detail");
+
+        const text = document.createElement("p");
+        text.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, quia.`;
+
+        subDetailLeft.appendChild(text);
+        leftBox.appendChild(subDetailLeft);
+      }
+    }
+
+    for (let count = 0; count < 2; count++) {
+      const subDetailRight = document.createElement("div");
+      subDetailRight.classList.add("sub-detail");
+
+      const text = document.createElement("p");
+      text.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, quia.`;
+
+      subDetailRight.appendChild(text);
+      rightBox.appendChild(subDetailRight);
+    }
+  });
+}
+
+function createHandlerButton() {
+  const bodyBox = document.querySelector(".body-box");
+
+  const handler = document.createElement("div");
+  handler.classList.add("handler");
+
+  const prevBtn = document.createElement("button");
+  prevBtn.type = "botton";
+  prevBtn.id = "prev-btn";
+  prevBtn.innerHTML = `<i class="ri-arrow-left-s-fill"></i> Prev`;
+
+  const nextBtn = document.createElement("button");
+  nextBtn.type = "botton";
+  nextBtn.id = "next-btn";
+  nextBtn.innerHTML = `Next <i class="ri-arrow-right-s-fill"></i>`;
+
+  handler.appendChild(prevBtn);
+  handler.appendChild(nextBtn);
+
+  bodyBox.appendChild(handler);
+}
+
 // -------------------- Pages Management --------------------
 let pagesCount = atom(0);
 
